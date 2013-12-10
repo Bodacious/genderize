0c9118711411e9ef2553dad0ff4e124da49fe83a
Bodacious <gavin@katanacode.com>
2013-12-10 11:55:04 +0000

    Updated development environment and fixed specs

--- genderize.gemspec
+++ genderize.gemspec
@@ -18,9 +18,8 @@ Gem::Specification.new do |s|
   
   s.test_files = Dir["spec/**/*"]
 
-  s.add_dependency "rails", "~> 3.2.13"
+  s.add_dependency "rails", ">= 3.2.13"
 
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "rspec-rails"
-  
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -9,15 +9,15 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130506080641) do
+ActiveRecord::Schema.define(version: 20130506080641) do
 
-  create_table "users", :force => true do |t|
-    t.string   "name",       :limit => 20, :null => false
-    t.string   "gender",     :limit => 1,  :null => false
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
+  create_table "users", force: true do |t|
+    t.string   "name",       limit: 20, null: false
+    t.string   "gender",     limit: 1,  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
