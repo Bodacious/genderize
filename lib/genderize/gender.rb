9d1938b559a7247587b5bd476177abf358971033
Bodacious <gavin@gavinmorrice.com>
2016-05-27 14:58:44 +0100

    Fixed behavior for blank genders
    Gender methods now behave the same way whether there is a value or not.
    This allows the developer to customize the behavior if the gender is not know.

--- lib/genderize/gender.rb
+++ lib/genderize/gender.rb
@@ -1,67 +1,72 @@
 module Genderize
   class Gender
-  
+
     include I18n
-    
+
+    # Maps the gender abbreviation name to the full name for translation keys
+    ABR_KEY_NAME_MAPPING = { "" => "blank", "m" => "masculine", "f" => "feminine" }
+
+
     attr_reader :abbr
-    
+
     def initialize(abbr)
       unless abbr.blank? or abbr.to_s =~ /\A(f|m|female|male)\Z/i
-        raise "Invalid abbreviation: '#{abbr}'" 
+        raise "Invalid abbreviation: '#{abbr}'"
       end
-      @abbr = abbr.blank? ? nil : abbr.to_s.first.downcase
+      @abbr = abbr.blank? ? '' : abbr.to_s.first.downcase
     end
-    
+
     def name
       @name ||= translation_for("name")
     end
-  
+
     def subject
       @subject ||= translation_for("subject")
     end
-    
+
     def object
       @object ||= translation_for("object")
     end
-    
+
     def possessive
       @possessive ||= translation_for("possessive")
     end
-    
+
     def casual
       @casual ||= translation_for("casual")
     end
-    
+
     def capital_abbr
       abbr.capitalize
     end
-  
+
     def male?
       abbr == 'm'
     end
-  
+
     def female?
       abbr == 'f'
     end
-  
+
+    def blank?
+      abbr == ""
+    end
+
     def to_s
       abbr
     end
-  
+
     def ==(val)
       abbr.to_s == val.to_s
     end
-  
+
+
     private
-    
+
+
     def translation_for(key)
-      case
-      when male? then I18n.t("genderize.#{key}.masculine")
-      when female? then I18n.t("genderize.#{key}.feminine")
-      else
-        nil
-      end
+      I18n.t("genderize.#{key}.#{ABR_KEY_NAME_MAPPING[abbr]}")
     end
-    
+
   end
 end
